e4664c35603d76a872801c5099b0aeaf52497945
Childcity <skulazkiy@gmail.com>
2019-07-20 15:07:46 +0300

    write /about slash command

--- CS_EventsNotifierSlackBot/RouteModules/SlashCommands/AboutModul.cs
+++ CS_EventsNotifierSlackBot/RouteModules/SlashCommands/AboutModul.cs
@@ -1,4 +1,6 @@
 ﻿using Nancy;
+using SlackBotMessages;
+using SlackBotMessages.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,26 @@ using System.Threading.Tasks;
 
 namespace CS_EventsNotifierSlackBot.RouteModules.SlashCommands {
 	public class AboutModul: NancyModule {
-		public AboutModul() {
-			//Post["/login", (ctx) => ctx.Request.Form.remember] = _ =>
-			//{
-			//	return "Handling code when remember is true!";
-			//};
+		public AboutModul(): base("/slash-cmd") {
+			Post("/about", _ => {
+				var message = new Message {
+					Text = "Hello, I can notify you about new events, that ocure on StopNet4 system.",
+					Attachments = new List<Attachment> {
+						new Attachment {
+							Fallback = "This is for slack clients which choose not to display the attachment.",
+							Pretext = "Below you can see my commands:",
+							Color = "good",
+							Fields = new List<Field> {
+								new Field {
+									Title = Emoji.Question + " /about",
+									Value = "Explain, what can this bot do. (Send this message)"
+								}
+							}
+						}
+					}
+				};
+				return message;
+			});
 		}
 	}
 }
